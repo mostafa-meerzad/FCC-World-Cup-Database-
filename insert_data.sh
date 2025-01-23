06ec090112051c89e2fb356e6291bb0bd83fa6c2
mostafa <mostafameerzad@gmail.com>
2025-01-23 14:02:23 +0430

    Create database if doesn't exists

--- insert_data.sh
+++ insert_data.sh
@@ -4,6 +4,8 @@ if [[ $1 == "test" ]]
 then
   PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
 else
+  # Create worldcup database if it doesn't esists
+  psql --username=postgres -tc "SELECT 1 FROM pg_database WHERE datname='worldcup';" | grep -q 1 || psql --username=postgres -c "CREATE DATABASE worldcup;"
   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
 fi
 
